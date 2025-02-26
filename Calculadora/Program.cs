415f56258e91c5c7d26321ea00dd003fce1cfe9c
LeonardoDecco <leonardo720xd@hotmail.com>
2025-02-26 19:58:55 -0300

    Alteraçao parte inicial

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Olá, qual operação deseja calcular: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
+            
+            Console.ReadLine();
+
+            int operacao = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado
+
+
         }
     }
 }
