1b702a50d1caf3991cef333ec628be9419f687b2
clemente-jey <jonasclemente.contato@gmail.com>
2025-02-26 20:04:32 -0300

    Operações de multiplicação e divisão

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int selecao;
+            double primeiroNumero;
+            double segundoNumero;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            if (selecao == 3) { primeiroNumero* segundoNumero; }
+
+            if (selecao == 4) { primeiroNumero / segundoNumero; }
+
+
         }
     }
-}
