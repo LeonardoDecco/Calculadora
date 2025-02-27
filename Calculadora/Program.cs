894deeae7ba1788beeeb3ff4a1ba9ce9e2ea4ffe
clemente-jey <jonasclemente.contato@gmail.com>
2025-02-27 18:47:26 -0300

    Alterei o código para ficar no mesmo padrão que minha dupla

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,28 +1,37 @@
-﻿namespace Calculadora
+﻿using System;
+
+namespace Calculadora
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            int operacao;
-            double num1;
-            double num2;
+            Console.WriteLine("Olá, qual operação deseja calcular: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
 
-            Console.WriteLine("Insira o número referente a operação: ");
-            int operacao = Console.ReadLine();
+            Console.ReadLine();
 
+            int operacao = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro numero: ");
+            int num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o primeiro número: ");
-            double num1 = Console.ReadLine();
+            Console.WriteLine("Digite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o segundo número: ");
-            double num2 = Console.ReadLine();
+            int resultado
 
-            if (operacao == 3) {"O resultado da multiplicação é igual a "+num1*num2; }
-
-            if (operacao == 4) {"O resultado da divisão é igual a "+num1/num2; }
-
-
-        }
+            if (escolha == 3)
+            {
+                resultado = num1 * num2
+               Console.WriteLine($"O resultado da adição é: {resultado}");
+            }
+            if (escolha == 4)
+            {
+                resultado = num1 / num2
+             Console.WriteLine($"O resultado da subtração é: {resultado}");
+            }
     }
