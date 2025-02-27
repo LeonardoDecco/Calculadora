591388e729ed743100905d722bd018328312918a
LeonardoDecco <leonardo720xd@hotmail.com>
2025-02-26 21:27:19 -0300

    atualizacao juncao com a master

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Olá, qual operação deseja calcular: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
+
+            Console.ReadLine();
+
+            int operacao = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado
+
             if (escolha == 1)
             {
                 resultado = num1 + num2
